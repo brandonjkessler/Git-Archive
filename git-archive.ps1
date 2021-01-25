6104800d529e1fe62c707a3c63eabd0eb09c2c50
Brandon Kessler <brandon.kessler@state.co.us>
2021-01-25 14:51:02 -0700

    Added section to add tags to output file.

--- git-archive.ps1
+++ git-archive.ps1
@@ -7,8 +7,15 @@ $curLoc = Get-Location
 set-location -Path $Path
 
 $output = "..\"
-$file = split-path ".\" -leaf
-
+$file = split-path ".\" -leaf ## Name the file the same as the folder
+$tag = (git tag -l) | sort-object -Descending ## Test for versions
+if($null -eq $tag){ ## If no tags
+    $file = split-path ".\" -leaf
+} elseif(($tag[0].length) -lt 3) { ## if only one tag
+    $file = "$(split-path ".\" -leaf)_" + "$($tag)"
+} else { ## Multiple tags will name with the latest
+    $file = "$(split-path ".\" -leaf)_" + "$($tag[0])"
+}
 powershell.exe -ExecutionPolicy Bypass -Command "git archive --format=zip $Branch --output=$output\$file.zip -0 ."
 
 Set-Location -Path $curLoc
